4da79b2860cc2572555c9e9672f7170e0548fd32
ssansone <35495609+ssansone@users.noreply.github.com>
2020-12-08 22:38:46 -0600

    Update #3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todo from './Todo';
+import NewTodo from './NewTodo';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
 <div className = "App">
 <h1> Samantha's To-Do List: </h1>
 <section id="myTodos">
+<NewTodo />
 <Todo />
       </section>
 
