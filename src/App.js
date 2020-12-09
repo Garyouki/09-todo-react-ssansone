3d7e6ce101c8fac69960c52dba70182ba09fd500
ssansone <35495609+ssansone@users.noreply.github.com>
2020-12-08 22:13:53 -0600

    Update #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todo from './Todo';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Samantha's ToDo App</h1>
-      </div>
+      
+<div className = "App">
+<h1> Samantha's To-Do List: </h1>
+<section id="myTodos">
+<Todo />
+      </section>
+
+</div>
+
     );
   }
 }
---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+class Todo extends Component {
+  render() {
+    return (
+      
+<div id="01234" className="todo">
+        <button className="complete">Check</button>
+        <p>Do A thing</p>
+      </div>
+    );
+  }
+}
+
+export default Todo;
