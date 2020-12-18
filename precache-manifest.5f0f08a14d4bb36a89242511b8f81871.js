1af9628181a81488a3ccc5fd9b55618b2f2fca22
ssansone <35495609+ssansone@users.noreply.github.com>
2020-12-17 19:39:36 -0600

    Updates

---
+++ precache-manifest.5f0f08a14d4bb36a89242511b8f81871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7af28ff0fe4b0cbd3e20",
+    "url": "/09-todo-react-ssansone/static/css/main.c5536852.chunk.css"
+  },
+  {
+    "revision": "7af28ff0fe4b0cbd3e20",
+    "url": "/09-todo-react-ssansone/static/js/main.7af28ff0.chunk.js"
+  },
+  {
+    "revision": "939cbde5f9c76620fa49",
+    "url": "/09-todo-react-ssansone/static/js/1.939cbde5.chunk.js"
+  },
+  {
+    "revision": "f6d81e2a434e154e137f",
+    "url": "/09-todo-react-ssansone/static/js/runtime~main.f6d81e2a.js"
+  },
+  {
+    "revision": "f6aaf34b26735a463ffff34b481781f4",
+    "url": "/09-todo-react-ssansone/index.html"
+  }
+];
